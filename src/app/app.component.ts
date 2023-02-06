d1a0e0b5fc9125f2ab554660ca72ba7771bdaa47
Heorhii Pluhovyi <heorhiipluhovui@gmail.com>
2023-02-06 19:53:21 +0200

    Initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import {
+  CdkDragDrop,
+  moveItemInArray,
+  transferArrayItem
+} from '@angular/cdk/drag-drop';
+import { Column } from './dragDrop.interface';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +13,58 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Kanban';
+
+  dragDropColumns: Column[] = [
+    new Column(
+      'todo',
+      [
+        'Get to work',
+        'Pick up groceries',
+        'Go home',
+        'Fall asleep'
+      ]
+    ),
+    new Column(
+      'done',
+      [
+        'Get up',
+        'Brush teeth',
+        'Take a shower',
+         'Check e-mail',
+        'Walk dog'
+      ]
+    ),
+    new Column(
+      'ideas',
+      [
+        'Create system for rocessing of documents in the army'
+      ]
+    ),
+    new Column(
+      'research',
+      ['Type of doc. forms used in army']
+    )
+  ]
+
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
+    }
+  }
+
+  addClass(i: number): string {
+    console.log(i)
+    return `grid__item--${3 * i + 1}-${3 * (i + 1)}`
+  }
 }
---
+++ src/app/dragDrop.interface.ts
@@ -0,0 +1,9 @@
+export class Column {
+  name: string;
+  list: string[];
+
+  constructor(name: string, list: string[]) {
+    this.name = name;
+    this.list = list;
+  }
+}
